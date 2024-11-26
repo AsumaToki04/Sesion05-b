0a54a60c33476557560bba1d796c47966e200161
AsumaToki04 <megamanzxu@hotmail.com>
2024-11-26 03:47:47 -0800

    Initial Commit

---
+++ Sesion05-b/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sesion05-b
+//
+//  Created by Asuma Toki on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sesion05-b/Sesion05_bApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesion05_bApp.swift
+//  Sesion05-b
+//
+//  Created by Asuma Toki on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesion05_bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
